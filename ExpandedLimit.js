1856fe535f2823d9d0cbc2bb13c26a9e458ef6bf
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-10-08 17:50:07 +0500

    Update ExpandedLimit.js

--- ExpandedLimit.js
+++ ExpandedLimit.js
@@ -7,12 +7,8 @@ class ExpandedLimit
 		if(k > 1.79E308 || layer >= 1)
 		{
 			k = "E".repeat(layer-1) + (mantissa + "E" + exponent)
-		} else if (layer < 1 && layer != 0)
-		{
-			k = 1/(new ExpandedLimit(mantissa,exponent,-layer));
-			if (k < 1E-308) {
-				k = 0;
-			}
+		} else if (k < 1E-324) {
+			k = 0;
 		}
 		return k;
 	}
